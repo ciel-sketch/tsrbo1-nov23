08bbb8f47a053de166d302690919805f1c85a4f1
Célia DANTEZ <celia.da@helloasso.org>
2023-11-27 12:56:24 +0100

    dev: added lines

--- front/main.js
+++ front/main.js
@@ -1,5 +1,5 @@
 console.log("start");
-const svgns = "http://www.w3.org/2000/svg"
+const svgns = "http://www.w3.org/2000/svg";
 
 const samples = 10;
 const r = 1;
@@ -8,24 +8,44 @@ const cx0 = 50;
 const cy0 = 50;
 const r0 = 45;
 
-const container = document.querySelector("svg .samples");
-const getSamplePosition = (i, samples) => {
-    const angle = i * 2* Math.PI / samples;
-    const x = cx0 + r0*Math.cos(angle);
-    const y = cy0 + r0*Math.sin(angle);
+const multiplicationFactor = 2;
 
-    return { x, y };
-}
+const container = document.querySelector("svg .samples");
+console.log("container: ", container);
+const getSamplePosition = (i, samples) => {
+  const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
+  const x = cx0 + r0 * Math.cos(angle);
+  const y = cy0 + r0 * Math.sin(angle);
+
+  return { x, y };
+};
 
 for (let i = 0; i < samples; i++) {
-    const elt = document.createElementNS(svgns, "circle")
-    console.log('elt: %O', elt);
+  const elt = document.createElementNS(svgns, "circle");
+  console.log("elt: %O", elt);
 
-    const {x: cx, y: cy} = getSamplePosition(i, samples);
-    console.log(cx);
+  const { x: cx, y: cy } = getSamplePosition(i, samples);
+  console.log(cx);
 
-    elt.setAttributeNS(null, "cx", cx);
-    elt.setAttributeNS(null, "cy", cy);
-    elt.setAttributeNS(null, "r", r);
-    container.appendChild(elt);
+  elt.setAttributeNS(null, "cx", cx);
+  elt.setAttributeNS(null, "cy", cy);
+  elt.setAttributeNS(null, "r", r);
+  container.appendChild(elt);
+}
+
+const lineContainer = document.querySelector("svg .lines");
+console.log("lineContainer: ", lineContainer);
+for (let i = 0; i < samples; i++) {
+  const elt = document.createElementNS(svgns, "line");
+  console.log("elt: %O", elt);
+
+  const p1 = getSamplePosition(i, samples);
+  const p2 = getSamplePosition(i * multiplicationFactor, samples);
+
+  elt.setAttributeNS(null, "x1", p1.x);
+  elt.setAttributeNS(null, "x2", p2.x);
+  elt.setAttributeNS(null, "y1", p1.y);
+  elt.setAttributeNS(null, "y2", p2.y);
+
+  lineContainer.appendChild(elt);
 }
