569ba6418cb199c29031b817fbbb398122c4e251
Célia DANTEZ <celia.da@helloasso.org>
2023-11-27 16:21:28 +0100

    dev: adds interface Point

--- front/src/math.ts
+++ front/src/math.ts
@@ -1,6 +1,7 @@
 import { cx0, r0, cy0 } from "./constants";
+import { Point } from "./interfaces/Point";
 
-export const getSamplePosition = (i: number, samples: number) => {
+export const getSamplePosition = (i: number, samples: number): Point => {
     const angle = (i * 2 * Math.PI) / samples - Math.PI / 2;
     const x = cx0 + r0 * Math.cos(angle);
     const y = cy0 + r0 * Math.sin(angle);
