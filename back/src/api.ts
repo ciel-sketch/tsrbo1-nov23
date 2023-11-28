f73b3e5f0867b60d8ec8070043f4b1641bc801d7
Célia DANTEZ <celia.da@helloasso.org>
2023-11-28 16:08:27 +0100

    dev: back adds random

--- back/src/api.ts
+++ back/src/api.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import { Config } from "./interfaces/Config";
+import { random } from "./utils";
 
 const app = express.Router();
 
-app.get("/random-config", (req, res) => {
-  res.json({
-    samples: 14,
-    multiplicationFactor: 25,
-  });
-});
+const randomConfig = (req: express.Request, res: express.Response): void => {
+  const config: Config = {
+    samples: random(0, 200),
+    multiplicationFactor: random(0, 100, 2),
+  };
+  res.json(config);
+};
+
+app.get("/random-config", randomConfig);
 
 export default app;
--- back/src/server.ts
+++ back/src/server.ts
@@ -10,7 +10,13 @@ const publicDir = ".";
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use("/api", api);
+
 app.use(express.static(publicDir));
 app.use(serveIndex(publicDir, { icons: true }));
 
