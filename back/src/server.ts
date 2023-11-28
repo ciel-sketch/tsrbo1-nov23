068a52d9f55062e6a0137aa6aa61ef81686063e7
Célia DANTEZ <celia.da@helloasso.org>
2023-11-28 15:18:33 +0100

    dev: back adds nodemon and ts-node

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,10 +1,15 @@
 console.log("About to start a server...");
 
 const express = require("express");
+const serveIndex = require("serve-index");
 
 const port = 3000;
+const publicDir = ".";
 
 const app = express();
+
+app.use(express.static(publicDir));
+app.use(serveIndex(publicDir, { icons: true }));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
